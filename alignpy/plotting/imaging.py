ede3577b21e03745b96d4a138ae4b2d0be5d3c36
Annika Salmi <69477753+annikasalmi@users.noreply.github.com>
2021-12-07 20:53:36 -0500

    Update imaging.py

--- alignpy/plotting/imaging.py
+++ alignpy/plotting/imaging.py
@@ -35,7 +35,7 @@ from search import *
 from align import *
 from inputoutput import *
 
-def plot(filename, objectname, pmax=97.5,z=10e-8,zoom=1):
+def plot(filename, objectname, pmax=97.5,z=10e-8,zoom=1): 
     '''
     Plots an astronomical image both zoomed in and zoomed out. The user can set how much to zoom in and also what scale to be zoomed in.
     
@@ -51,28 +51,35 @@ def plot(filename, objectname, pmax=97.5,z=10e-8,zoom=1):
         The amount to scale the image by. Default is 97.5.
         
     z: float
-        Redshift of a given astronomical object. If not given, default is 0.
+        Redshift of a given astronomical object. If not given, default is 10e-8 (essentially 0 but avoids divide by zero error).
     
     zoom: float
         The amount to zoom in on the image. Default is 1.
     '''
-    image = fits.open(filename, ignore_missing_end=True)
+    hdu = fits.open(filename)
     ra,dec,radius = resolve(objectname)
-    hdr = image[0].header  
+    
+    hdr = hdu[0].header
+   
     try:
         try:
-            filtername = hdr['FILTER']
-        except:
-            filtername = hdr['FILTER2']
+            try: # checking that the header exists
+                filtername = str(hdr['FILTER'])
+
+            except:
+                filtername = str(hdr['FILTER2'])
+
+        except: 
+            hdr = hdu[1].header
+            try:
+                filtername = str(hdr['FILTER'])
+
+            except:
+                filtername = str(hdr['FILTER2'])
     except:
-        filtername = 'No filter name in FITS file'
-        
-    if type(image[0].data) != np.ndarray:
-        hdr = image[1].header 
-        if type(image[1].data) != np.ndarray:
-            filtername = hdr['FILTER2']
-    
-    image.close()
+            filtername = 'No filter in FITS file'
+            
+    hdu.close()
     
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
     asecperkpc=cosmo.arcsec_per_kpc_comoving(z).value
@@ -80,22 +87,19 @@ def plot(filename, objectname, pmax=97.5,z=10e-8,zoom=1):
     fig = plt.figure(figsize=(15, 7))
 
     f1 = aplpy.FITSFigure(filename, hdu=1, north=True, figure=fig)
-    if z == 10e-8:
-        print("Scaled as if redshift were 0.")
     f1.recenter(ra,dec,radius=radius/zoom*asecperkpc/3600.)
-    
     f1.set_title(objectname,size=24, weight='heavy',color='black', loc='left',family='serif')
-
-    f1.add_label(0.05, 0.9, 'Filter: 'filtername, relative=True,size=20,
+    f1.add_label(0.05, 0.9, 'Filter: '+filtername, relative=True,size=20,
                  weight='heavy',color='red', horizontalalignment='left',family='serif')
-
     f1.show_colorscale(pmin=0,pmax=pmax,cmap='gray')#,stretch='arcsinh')
     
-    theta = 5/3600. * u.arcsec                   # angle
-    r_ang = Planck15.kpc_proper_per_arcmin(z) # phys. dist. per angle
-    r     = r_ang * theta * 60                    # physical distance
+    if z == 10e-8:
+        print("Scaled as if redshift were 0.")
     
-    if z != 0:
+    if z != 10e-8:
+        theta = 5/3600. * u.arcsec                   # angle
+        r_ang = Planck15.kpc_proper_per_arcmin(z) # phys. dist. per angle
+        r = r_ang * theta * 60                    # physical distance
         f1.add_scalebar(5./3600.)
         f1.scalebar.set_label(str('5" or ')+ str(round(r.value,3)*1000) +" pc")
         f1.scalebar.set_font(size=20,weight='heavy',family='serif')
@@ -120,18 +124,18 @@ def combine(fitsfilter1,fitsfilter2,method):
         The location of the file to use as a second file to multiply.  
     '''
     # only run this once
-    if method != 'divide' & 'add' &'subtract' &'multiply'&'mean':
-        print('Method specified does not exist.')
+    while method not in ['add', 'subtract', 'multiply','divide','mean']:
+        return 'Method specified does not exist.'
     
     if os.path.isfile(fitsfilter2.split('.')[0]+method+'_combined_image.fits') is False:
         
         # the second image gets shifted
-        pixelrange=20
+        pixelrange=30
 
         # this shifts the second file and saves a new image
         # in my research want to do IR/optical, shift_save returns optical image
         if os.path.isfile(fitsfilter2.split('.')[0]+'_shifted.fits') is False:
-            shift_save(fitsfilter1,fitsfilter2,pixelrange,fitsfilter2.split('.')[0]+'_shifted.fits')
+            chi2align.shift_save(fitsfilter1,fitsfilter2,pixelrange,fitsfilter2.split('.')[0]+'_shifted.fits')
 
         print('Shifted the image')
 
@@ -140,7 +144,7 @@ def combine(fitsfilter1,fitsfilter2,method):
         if type(image1.data) != np.ndarray:
             image1 = hdu1[1]
         elif type(image1.data) != np.ndarray:
-            print('Cannot open FITS file',fitsfilter1,'with extension 1 or 0')
+            return 'Cannot open FITS file',fitsfilter1,'with extension 1 or 0'
 
         hdu2 = fits.open(fitsfilter2.split('.')[0]+'_shifted.fits',ignore_missing_end=True)
         image2 = hdu2[0]
@@ -188,6 +192,9 @@ def combine(fitsfilter1,fitsfilter2,method):
 
         return fitsfilter2.split('.')[0]+method+'_combined_image.fits'
     
+    else:
+        return fitsfilter2.split('.')[0]+method+'_combined_image.fits'
+    
 def combined_plot(fitsfilter1,fitsfilter2, objectname, method = None, pmax=97.5,z=10e-8,zoom=1):
     '''
     Plots an astronomical image both zoomed in and zoomed out. The user can set how much to zoom in and also what scale to be zoomed in.
@@ -219,23 +226,20 @@ def combined_plot(fitsfilter1,fitsfilter2, objectname, method = None, pmax=97.5,
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
     asecperkpc=cosmo.arcsec_per_kpc_comoving(z).value
     
-    ra,dec,radius = resolve(objectname)
+    ra,dec,radius = search.resolve(objectname)
     
     fileresult = combine(fitsfilter1,fitsfilter2,method)
     f1 = aplpy.FITSFigure(fileresult, hdu=0, north=True, figure=fig)
+    f1.recenter(ra,dec,radius=radius/zoom*asecperkpc/3600.)
+    f1.set_title(objectname,size=24, weight='heavy',color='black', loc='left',family='serif')
+    f1.add_label(0.05, 0.9, method, relative=True,size=20,
+                 weight='heavy',color='red', horizontalalignment='left',family='serif')
+    f1.show_colorscale(pmin=0,pmax=pmax,cmap='hot')#,stretch='arcsinh')
+
     if z == 10e-8:
         print("Scaled as if redshift were 0.")
-        f1.recenter(ra,dec,radius=radius/zoom*asecperkpc/3600.)
-        f1.set_title(objectname,size=24, weight='heavy',color='black', loc='left',family='serif')
-        f1.add_label(0.05, 0.9, method, relative=True,size=20,
-                     weight='heavy',color='red', horizontalalignment='left',family='serif')
-        f1.show_colorscale(pmin=0,pmax=pmax,cmap='gray')#,stretch='arcsinh')
 
-        theta = 5/3600. * u.arcsec                   # angle
-        r_ang = Planck15.kpc_proper_per_arcmin(z) # phys. dist. per angle
-        r     = r_ang * theta * 60                    # physical distance
-
-    if z != 0:
+    if z != 10e-8:
         theta = 5/3600. * u.arcsec                   # angle
         r_ang = Planck15.kpc_proper_per_arcmin(z) # phys. dist. per angle
         r     = r_ang * theta * 60                    # physical distance
